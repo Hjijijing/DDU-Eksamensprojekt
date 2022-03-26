c615a6eedb9cad9368bf02f2d60bc6a5b3845171
Hjijijing <rashansen69@gmail.com>
2022-03-26 13:32:40 +0100

    Startkraft til partikler
    Så de bevæger sig lidt

--- Assets/Scripts/ParticleS/ParticleScript.cs
+++ Assets/Scripts/ParticleS/ParticleScript.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public abstract class ParticleScript : MonoBehaviour
 {
+    [SerializeField] float maxInitialForce = 1000f;
+
+    private void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        Vector2 force = new Vector2(Random.Range(-maxInitialForce, maxInitialForce), Random.Range(-maxInitialForce, maxInitialForce));
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
 
 
-   
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.TryGetComponent<AtomScript>(out AtomScript player))
